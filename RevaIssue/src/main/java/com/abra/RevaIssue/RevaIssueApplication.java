dea4285d187a18d15c833e44774089f5b64a39ef
armandohimself <hey@armandohimself.com>
2025-12-16 15:03:26 -0600

    Init commit

---
+++ RevaIssue/src/main/java/com/abra/RevaIssue/RevaIssueApplication.java
@@ -0,0 +1,13 @@
+package com.abra.RevaIssue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevaIssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevaIssueApplication.class, args);
+	}
+
+}
---
+++ RevaIssue/src/test/java/com/abra/RevaIssue/RevaIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abra.RevaIssue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevaIssueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
