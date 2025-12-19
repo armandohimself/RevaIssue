b540e4dd4e3b0be68c3fd49016a7570fabf1d639
Byron Sophin <Sophinbyron03@gmail.com>
2025-12-19 16:48:13 -0500

    small changes

--- RevaIssue/src/main/java/com/abra/revaissue/entity/Issue.java
+++ RevaIssue/src/main/java/com/abra/revaissue/entity/Issue.java
@@ -1,10 +1,33 @@
 package com.abra.revaissue.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
 @Entity
 @Table(name = "issue")
 public class Issue {
+
+    @Id
+    @Column(name = "issue_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID issueId;
+    @Column
+    private String name;
+    @Column
+    private String description;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project projectId;
+
 
 }
