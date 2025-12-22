7be8d7bb0ffe7bbf1a9b2b2abd34bbe5421ddfb5
Byron Sophin <Sophinbyron03@gmail.com>
2025-12-22 16:59:00 -0500

    Actual implementation of issue service and docstring added to issue repository

--- RevaIssue/src/main/java/com/abra/revaissue/repository/IssueRepository.java
+++ RevaIssue/src/main/java/com/abra/revaissue/repository/IssueRepository.java
@@ -11,20 +11,20 @@ import java.util.List;
 import java.util.UUID;
 
 public interface IssueRepository extends JpaRepository<Issue, UUID> {
-    //All issues for a project
+    /**Returns all issues for a project*/
     List<Issue> findByProject_ProjectId(UUID projectId);
-    //All issues for a project with a certain status
+    /**Returns all issues for a project with a specified status*/
     List<Issue> findByProject_ProjectIdAndStatus(UUID projectId, IssueStatus status);
-    //All issues for a project with a certain severity
+    /**Returns all issues for a project with a specified severity*/
     List<Issue> findByProject_ProjectIdAndSeverity(UUID projectId, IssueSeverity severity);
-    //All issues for a project with a certain priority
+    /**Returns all issues for a project with a specified priority*/
     List<Issue> findByProject_ProjectIdAndPriority(UUID projectId, IssuePriority priority);
-    //All issues made by a user
+    /**Returns all issues made by a user*/
     List<Issue> findByCreatedBy_UserId(UUID userId);
-    //All issues assigned to a user
+    /**Returns all issues assigned to a user*/
     List<Issue> findByAssignedTo_UserId(UUID userId);
-    //All issues assigned to a user for a certain project
+    /**Returns all issues assigned to a user for a specified project*/
     List<Issue> findByProject_ProjectIdAndAssignedTo_UserId(UUID projectId, UUID userId);
-    //All issues created by a user for a certain project
+    /**Returns all issues created by a user for a specified project*/
     List<Issue> findByProject_ProjectIdAndCreatedBy_UserId(UUID projectId, UUID userId);
 }
