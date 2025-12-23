8e4863d4642ecce888b8186d630a67213a1f55cd
armandohimself <hey@armandohimself.com>
2025-12-22 19:39:49 -0600

    Initial table annotation scaffolding

--- RevaIssue/src/main/java/com/abra/revaissue/entity/Project.java
+++ RevaIssue/src/main/java/com/abra/revaissue/entity/Project.java
@@ -1,27 +1,36 @@
 package com.abra.revaissue.entity;
 
+import java.time.Instant;
+import java.util.UUID;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.UUID;
-
-@Data
 @Entity
-@Table(name = "project")
-@NoArgsConstructor
+@Table(name = "projects")
 public class Project {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(name = "project_id", nullable = false)
+    @Column(name = "project_id")
     private UUID projectId;
-    @Column
+    @Column(name="project_name")
     private String projectName;
+    @Column(name = "project_description")
+    private String projectDescription;
+    // private enum projectStatus;
+    // private interface of members: projectMembers[]
+    @Column(name = "created_by_user_id")
+    private UUID createdByUserId;
+    @Column(name = "archived_by_user_id")
+    private UUID archivedByUserId;
+    @Column(name = "status_updated_by_user_id")
+    private UUID statusUpdatedByUserId;
+    @Column(name = "created_at")
+    private Instant createdAt;
+    @Column(name = "archived_at")
+    private Instant archivedAt;
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
 }
