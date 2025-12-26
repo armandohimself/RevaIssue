85ae5c0e973456d633c86e54f88b56d53b2059a0
aguo312 <andrewguo312@gmail.com>
2025-12-26 13:02:00 -0500

    added basic jwt utility methods

--- RevaIssue/src/main/java/com/abra/revaissue/util/JwtUtility.java
+++ RevaIssue/src/main/java/com/abra/revaissue/util/JwtUtility.java
@@ -1,8 +1,44 @@
 package com.abra.revaissue.util;
 
+import java.util.Date;
+import java.util.UUID;
+
 import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtUtility {
+
+    private final String SECRET_KEY = "your-key-should-be-at-least-32-bytes";
+
+    public String generateAccessToken(UUID userId, String username) {
+        return Jwts.builder()
+                .subject(userId.toString())
+                .claim("username", username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 15 * 60 * 1000)) // 15 minutes
+                .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()), Jwts.SIG.HS256)
+                .compact();
+    }
+
+    public String extractId(String token) {
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("username", String.class);
+    }
 
 }
