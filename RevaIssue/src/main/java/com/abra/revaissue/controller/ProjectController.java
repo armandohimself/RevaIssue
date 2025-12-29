1cfee915491e2f8d51ed739c5daebc1cbbadb26f
armandohimself <hey@armandohimself.com>
2025-12-29 12:07:18 -0600

    Added UpdateProjectRequest DTO and implemented update method in ProjectService

--- RevaIssue/src/main/java/com/abra/revaissue/controller/ProjectController.java
+++ RevaIssue/src/main/java/com/abra/revaissue/controller/ProjectController.java
@@ -6,14 +6,18 @@ import java.util.UUID;
 import com.abra.revaissue.dto.project.CreateProjectRequest;
 import com.abra.revaissue.dto.project.ProjectMapper;
 import com.abra.revaissue.dto.project.ProjectResponse;
+import com.abra.revaissue.dto.project.UpdateProjectRequest;
 import com.abra.revaissue.entity.Project;
 import com.abra.revaissue.service.ProjectService;
 
 import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,7 +44,21 @@ public class ProjectController {
         return ResponseEntity.ok(ProjectMapper.toResponse(createdProject));
     }
 
+    // TODO: "/projects/{projectId}/access" also ProjectAccess table?
+
     // ! GET
+
+    @GetMapping
+    public ResponseEntity<List<ProjectResponse>> listAll() {
+
+        List<ProjectResponse> projectList = projectService.getAll()
+                .stream()
+                .map(ProjectMapper::toResponse)
+                .toList();
+
+        return ResponseEntity.ok(projectList);
+    }
+
     @GetMapping("/{projectId}")
     public ResponseEntity<ProjectResponse> getById(@PathVariable UUID projectId) {
 
@@ -48,19 +66,19 @@ public class ProjectController {
         return ResponseEntity.ok(ProjectMapper.toResponse(getProject));
     }
 
-    @GetMapping
-    public ResponseEntity<List<ProjectResponse>> listAll() {
-
-        List<ProjectResponse> projectList = projectService.getAll()
-            .stream()
-            .map(ProjectMapper::toResponse)
-            .toList();
-
-        return ResponseEntity.ok(projectList);
-    }
+    // TODO: "/projects/{userId}" instead? Maybe in ProjectAccess table tbh
+    @GetMapping("/{projectId}/{userId}")
 
     // ! PUT
+    @PatchMapping("/{projectId}")
+    public ResponseEntity<ProjectResponse> update(@PathVariable UUID projectId,
+            @RequestBody UpdateProjectRequest updateProjectRequest, @RequestHeader("X-User-Id") UUID userId) {
+        Project updatedProject = projectService.update(projectId, updateProjectRequest, userId);
+
+        return ResponseEntity.ok(ProjectMapper.toResponse(updatedProject));
+    }
 
     // ! "DELETE"
+    // @DeleteMapping("/{projectId}")
 
 }
--- RevaIssue/src/main/java/com/abra/revaissue/service/ProjectService.java
+++ RevaIssue/src/main/java/com/abra/revaissue/service/ProjectService.java
@@ -2,12 +2,15 @@ package com.abra.revaissue.service;
 
 import java.time.Instant;
 import java.util.List;
-// import java.util.Optional;
 import java.util.UUID;
 
+import com.abra.revaissue.dto.project.UpdateProjectRequest;
 import com.abra.revaissue.entity.Project;
+import com.abra.revaissue.entity.user.User;
 import com.abra.revaissue.enums.ProjectStatus;
+import com.abra.revaissue.enums.UserEnum.Role;
 import com.abra.revaissue.repository.ProjectRepository;
+import com.abra.revaissue.repository.UserRepository;
 
 import org.springframework.stereotype.Service;
 
@@ -15,12 +18,17 @@ import org.springframework.stereotype.Service;
 public class ProjectService {
 
     private final ProjectRepository projectRepository;
+    private final UserRepository userRepository;
+    // private final ProjectAccessRepository
 
-    public ProjectService(ProjectRepository projectRepository) {
+    public ProjectService(ProjectRepository projectRepository, UserRepository userRepository) {
         this.projectRepository = projectRepository;
+        this.userRepository = userRepository;
     }
 
-    // CREATE
+    // ! CREATE
+
+    // * Create project (Admin-only)
     public Project create(Project project) {
         // Guard rails
         if (project == null) {
@@ -35,7 +43,8 @@ public class ProjectService {
             throw new IllegalArgumentException("User's ID who created project cannot be null!");
         }
 
-        // Check User "Credentials"/Role? || is that done in the API level?
+        // ! Admin-only
+        checkIfRoleAdmin(project.getCreatedByUserId());
 
         Instant now = Instant.now();
 
@@ -54,54 +63,114 @@ public class ProjectService {
         return projectRepository.save(project);
     }
 
-    // READ
+    // ! READ
+
+    // * Get all projects as list
     public List<Project> getAll() {
+        // TODO: Update to grab views based on roles
         return projectRepository.findAll();
     }
 
+    // * Get specific project by it's Id.
     public Project getById(UUID projectId) {
-        // Could swap for Supplier function later
-        var opt = projectRepository.findById(projectId);
-
-        if (opt.isPresent()) {
-            return opt.get();
-        }
-
-        throw new IllegalArgumentException("Project not found!");
+        // TODO: Update to include userId
+        return checkIfProjectExists(projectId);
     }
 
+    // * Get list of projects by status
     public List<Project> getByStatus(ProjectStatus status) {
         return projectRepository.findByProjectStatus(status);
     }
 
-    // UPDATE
-    // public Project update(UUID projectId, UpdateProjectRequestDTO dto) {
+    // ! UPDATE
 
-    //     TODO: Need to create DTO
+    // * Update project details
+    public Project update(UUID projectId, UpdateProjectRequest updateProjectRequest, UUID userId) {
+        // Guard rails
+        if (projectId == null) throw new IllegalArgumentException("Project Id is required to update a project!");
 
-    //     Project project = getById(projectId);
+        if (updateProjectRequest == null) throw new IllegalArgumentException("updateProjectRequest is required to update a project!");
 
-    //     Update project
+        if (userId == null) throw new IllegalArgumentException("User Id is required to update a project!");
 
-    //     return project;
-    // }
-
-    public Project updateByStatus(UUID projectId, ProjectStatus newStatus, UUID statusUpdatedByUserId) {
-        Project project = getById(projectId);
-
+        // ! Admin-only
+        checkIfRoleAdmin(userId);
+        
+        Project project = checkIfProjectExists(projectId);
+        
         Instant now = Instant.now();
 
-        project.setProjectStatus(newStatus);
-        project.setStatusUpdatedByUserId(statusUpdatedByUserId);
+        // Patch Updates
+        applyPatchFields(project, updateProjectRequest);
+        applyStatusTransition(project, updateProjectRequest.projectStatus(), userId, now);
+
+        // Audit Timestamp
         project.setUpdatedAt(now);
 
-        // "DELETE"/ARCHIVE
-        if (newStatus == ProjectStatus.ARCHIVED) {
-            project.setArchivedAt(now);
-            project.setArchivedByUserId(statusUpdatedByUserId);
+        return projectRepository.save(project);
+    }
+
+    // ! DELETE
+
+    
+
+    // ! Helper Functions
+    public void checkIfRoleAdmin(UUID userId) {
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> new IllegalArgumentException("User not found!"));
+
+        if(user.getRole() != Role.ADMIN) {
+            throw new IllegalStateException("Admin privilege required to perform action!");
+        }
+    }
+
+    private Project checkIfProjectExists(UUID projectId) {
+        Project project = projectRepository.findById(projectId)
+            .orElseThrow(() -> new IllegalArgumentException("Project not found!"));
+
+        return project;
+    }
+
+    private void applyPatchFields(Project project, UpdateProjectRequest updateProjectRequest) {
+        // Guard rail 
+        
+        // * Project Name
+        if (updateProjectRequest.projectName() != null) {
+            if (updateProjectRequest.projectName().isBlank()) {
+                throw new IllegalArgumentException("projectName cannot be blank!");
+            }
+            // TODO: Create a record
+            project.setProjectName(updateProjectRequest.projectName());
         }
 
-        return projectRepository.save(project);
+        // * Project Description
+        if (updateProjectRequest.projectDescription() != null) {
+            // TODO: Create a record
+            project.setProjectDescription(updateProjectRequest.projectDescription());
+        }
+    }
+
+    private void applyStatusTransition(Project project, ProjectStatus newProjectStatus, UUID userId, Instant now) {
+        // Guard rail
+        if (newProjectStatus == null) return;
+        // no change, nothing to audit
+        if (newProjectStatus == project.getProjectStatus()) return; 
+
+        // * Project Status
+        // TODO: Create record of who made the change + update status change
+        project.setProjectStatus(newProjectStatus);
+        project.setStatusUpdatedByUserId(userId);
+
+        // * ARCHIVED & RE-ACTIVE Status
+        if (newProjectStatus == ProjectStatus.ARCHIVED) {
+            // TODO: Create a record
+            project.setArchivedByUserId(userId);
+            project.setArchivedAt(now);
+        } else if (newProjectStatus == ProjectStatus.ACTIVE) {
+            // TODO: Create a record
+            project.setArchivedByUserId(null);
+            project.setArchivedAt(null);
+        }
     }
 
 }
