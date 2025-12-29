ffcea55e618e67d42be15543931eddb59f5d0fa1
Byron Sophin <Sophinbyron03@gmail.com>
2025-12-29 08:44:05 -0500

    Implemented jwt token authorization for controller

--- RevaIssue/src/main/java/com/abra/revaissue/controller/IssueController.java
+++ RevaIssue/src/main/java/com/abra/revaissue/controller/IssueController.java
@@ -9,7 +9,9 @@ import com.abra.revaissue.enums.IssueStatus;
 import com.abra.revaissue.service.IssueService;
 import com.abra.revaissue.service.ProjectService;
 import com.abra.revaissue.service.UserService;
+import com.abra.revaissue.util.JwtUtility;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,20 +25,16 @@ public class IssueController {
     private UserService userService;
     @Autowired
     private ProjectService projectService;
+    @Autowired
+    private JwtUtility jwtUtility;
 
     @PostMapping("/projects/{projectId}/issues")
-    public Issue createIssue(@PathVariable UUID projectId, @RequestBody Issue issue){
+    public Issue createIssue(@PathVariable UUID projectId, @RequestBody Issue issue, @RequestHeader(name = "Authorization") String token){
+        String slicedToken = token.split(" ")[1];
+        UUID actingUserId = UUID.fromString(jwtUtility.extractId(slicedToken));
+        User actingUser = userService.getUserByUUID(actingUserId);
         Project project = projectService.getProjectById(projectId);
-        issue.setProject(project);
-        return issueService.createIssue(issue);
-    }
-    @PostMapping("/user")
-    public User createUser(@RequestBody User user){
-        return userService.createUser(user);
-    }
-    @PostMapping("/project")
-    public Project createProject(@RequestBody Project project){
-        return projectService.createProject(project);
+        return issueService.createIssue(issue, project, actingUser);
     }
     @GetMapping("/projects/{projectId}/issues")
     public List<Issue> getIssuesForProject(@PathVariable UUID projectId,
@@ -70,14 +68,28 @@ public class IssueController {
         return issueService.getIssuesCreatedByUser(user);
     }
     @PutMapping("/issues/{issueId}/assign/{userId}")
-    public Issue assignDeveloper(@PathVariable UUID issueId, @PathVariable UUID userId){
+    public Issue assignDeveloper(@PathVariable UUID issueId, @PathVariable UUID userId, @RequestHeader(name = "Authorization") String token){
+        String slicedToken = token.split(" ")[1];
+        UUID actingUserId = UUID.fromString(jwtUtility.extractId(slicedToken));
+        User actingUser = userService.getUserByUUID(actingUserId);
         Issue issue = issueService.getIssueById(issueId);
         User user = userService.getUserByUUID(userId);
         return issueService.assignDeveloper(issue, user);
     }
     @PutMapping("/issues/{issueId}/status")
-    public Issue updateIssueStatus(@PathVariable UUID issueId, @RequestParam IssueStatus status){
+    public Issue updateIssueStatus(@PathVariable UUID issueId, @RequestParam IssueStatus status, @RequestHeader(name = "Authorization") String token){
+        String slicedToken = token.split(" ")[1];
+        UUID actingUserId = UUID.fromString(jwtUtility.extractId(slicedToken));
+        User actingUser = userService.getUserByUUID(actingUserId);
         Issue issue = issueService.getIssueById(issueId);
-        return issueService.updateStatus(issue, status);
+        return issueService.updateStatus(issue, status, actingUser);
+    }
+    @DeleteMapping("/{issueId")
+    public ResponseEntity<?> deleteIssue(@PathVariable UUID issueId, @RequestHeader(name = "Authorization") String token){
+        String slicedToken = token.split(" ")[1];
+        UUID actingUserId = UUID.fromString(jwtUtility.extractId(slicedToken));
+        User actingUser = userService.getUserByUUID(actingUserId);
+        issueService.deleteIssue(issueId, actingUser);
+        return ResponseEntity.noContent().build();
     }
 }
