ea872ac6fcb66f79215f71a98ba9882547d7011b
Byron Sophin <Sophinbyron03@gmail.com>
2025-12-29 08:43:33 -0500

    Added role checks for crud operations

--- RevaIssue/src/main/java/com/abra/revaissue/service/IssueService.java
+++ RevaIssue/src/main/java/com/abra/revaissue/service/IssueService.java
@@ -6,11 +6,14 @@ import com.abra.revaissue.entity.user.User;
 import com.abra.revaissue.enums.IssuePriority;
 import com.abra.revaissue.enums.IssueSeverity;
 import com.abra.revaissue.enums.IssueStatus;
+import com.abra.revaissue.enums.UserEnum;
 import com.abra.revaissue.repository.IssueRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.AccessDeniedException;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -26,9 +29,14 @@ public class IssueService {
         this.issueRepository = issueRepository;
     }
     //CREATE
-    public Issue createIssue(Issue issue){
-        issue.setCreatedAt(java.time.LocalDateTime.now());
-        issue.setUpdatedAt(java.time.LocalDateTime.now());
+    public Issue createIssue(Issue issue, Project project, User actingUser){
+        UserEnum.Role role = actingUser.getRole();
+        if(role != UserEnum.Role.TESTER && role != UserEnum.Role.ADMIN){
+            throw new RuntimeException("Only TESTERS OR ADMINS can create issues");
+        }
+        issue.setCreatedAt(Instant.now());
+        issue.setUpdatedAt(Instant.now());
+        issue.setProject(project);
         return issueRepository.save(issue);
     }
     //SELECT/QUERIES
@@ -60,15 +68,31 @@ public class IssueService {
         return issueRepository.findById(issueId).orElseThrow(() -> new EntityNotFoundException("Issue not found with id: " + issueId));
     }
     //UPDATE
-    public Issue assignDeveloper(Issue issue, User user){
-        issue.setUpdatedAt(java.time.LocalDateTime.now());
+    public Issue assignDeveloper(Issue issue, User actingUser){
+        UserEnum.Role role = actingUser.getRole();
+        if(role != UserEnum.Role.TESTER && role != UserEnum.Role.ADMIN){
+            throw new RuntimeException("Only TESTERS OR ADMINS can assign issues");
+        }
+        issue.setUpdatedAt(Instant.now());
         issue.setAssignedTo(user);
         return issueRepository.save(issue);
     }
-    public Issue updateStatus(Issue issue, IssueStatus status){
+    public Issue updateStatus(Issue issue, IssueStatus status, User actingUser){
+        UserEnum.Role role = actingUser.getRole();
+        if(role != UserEnum.Role.TESTER && role != UserEnum.Role.ADMIN){
+            throw new RuntimeException("Only TESTERS OR ADMINS can set issue status");
+        }
         issue.setStatus(status);
-        issue.setUpdatedAt(LocalDateTime.now());
+        issue.setUpdatedAt(Instant.now());
         return issueRepository.save(issue);
     }
-
+    //DELETE
+    public void deleteIssue(UUID issueId, User actingUser){
+        Issue issue = issueRepository.findById(issueId).orElseThrow(() -> new EntityNotFoundException("Issue not found"));
+        UserEnum.Role role = actingUser.getRole();
+        if(role != UserEnum.Role.TESTER && role != UserEnum.Role.ADMIN){
+            throw new RuntimeException("Only TESTERS OR ADMINS can delete issues");
+        }
+        issueRepository.delete(issue);
+    }
 }
