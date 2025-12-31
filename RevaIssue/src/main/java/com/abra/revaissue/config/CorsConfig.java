084b4ac9a2af9c357460f52bf05f14091e5f676d
aguo312 <andrewguo312@gmail.com>
2025-12-31 09:43:21 -0500

    Added cors file

---
+++ RevaIssue/src/main/java/com/abra/revaissue/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.abra.revaissue.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    // You can add CORS mappings here if needed in the future
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+}
