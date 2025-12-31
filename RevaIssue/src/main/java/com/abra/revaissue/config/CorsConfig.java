60b4a60b16666764d5691928e8ff6b2cd339ada1
aguo312 <andrewguo312@gmail.com>
2025-12-31 14:10:27 -0500

    updated Cors configuration

--- RevaIssue/src/main/java/com/abra/revaissue/config/CorsConfig.java
+++ RevaIssue/src/main/java/com/abra/revaissue/config/CorsConfig.java
@@ -6,13 +6,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    // You can add CORS mappings here if needed in the future
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
