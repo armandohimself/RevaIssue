438868aee7bf5cb8a99ac3e846dac22641999aa8
KdRome <r.petliak12@gmail.com>
2026-01-05 23:14:17 -0500

    updated to include new routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login';
 import { authGuard } from './guards/auth/auth-guard';
 import { adminGuard } from './guards/admin/admin-guard';
+import { AddUserComponent } from './features/users/add-user/add-user';
+import { ListUsersComponent } from './features/users/list-users/list-users';
 
 export const routes: Routes = [
   {
@@ -22,12 +24,22 @@ export const routes: Routes = [
   },
   {
     path: 'admin/dashboard',
-    component: LoginComponent, // placeholder for now / admin dashboard
+    component: AddUserComponent, // placeholder for now / admin dashboard
     canActivate: [authGuard, adminGuard] // if auth is good, goes to check if user is admin
   },
   {
     path: 'user/dashboard',
     component: LoginComponent, // placeholder for now - tester / dev
     canActivate: [authGuard] // may need additional guards for tester / developer
+  },
+  {
+    path: 'admin/users/add',
+    component: AddUserComponent,
+    canActivate: [authGuard, adminGuard]
+  },
+  {
+    path: 'admin/users/get-all',
+    component: ListUsersComponent,
+    canActivate: [authGuard, adminGuard]
   },
 ];
