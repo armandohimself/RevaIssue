6505f990d345d2bad92e8d5d58ca9b5290e41d16
armandohimself <hey@armandohimself.com>
2026-01-10 00:45:30 -0600

    Got console logs to show in terminal

--- RevaIssue/src/test/java/com/abra/revaissue/RevaIssueApplicationTests.java
+++ RevaIssue/src/test/java/com/abra/revaissue/RevaIssueApplicationTests.java
@@ -1,10 +1,16 @@
 package com.abra.revaissue;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Duration;
 
 /**
  * This is your base test file.
@@ -17,16 +23,34 @@ class RevaIssueApplicationTests {
 		assertThat(2 + 2).isEqualTo(4);
 	}
 
-	public static void main(String[] args) {
-		ensureBrowserQuits();
-	}
-
-	public static void ensureBrowserQuits() {
+	@Test
+	void testSeleniumInputElement() {
 		WebDriver driver = null;
 		try {
 			driver = new ChromeDriver();
-			driver.get("https://practicetestautomation.com/practice-test-login/");
-			System.out.println(driver.getTitle());
+			driver.get("https://testautomationpractice.blogspot.com/");
+
+			//! GUI HEADER
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+			WebElement GUIElementsHeader = driver.findElement(By.linkText("GUI Elements"));
+			System.out.println(GUIElementsHeader.getText());
+
+			//! Name Input
+			WebElement inputName = driver.findElement(By.id("name"));
+
+			// Output context information about the input element
+			System.out.println("=== Input Name Element Context ===");
+			System.out.println("Tag: " + inputName.getTagName()); //! input
+			System.out.println("ID: " + inputName.getAttribute("id")); //! name
+			System.out.println("Placeholder: " + inputName.getAttribute("placeholder")); //Enter Name
+			System.out.println("Type: " + inputName.getAttribute("type")); //! text
+
+			inputName.click();
+			inputName.sendKeys("Armando Arteaga");
+
+			// Get the value that was entered
+			System.out.println("Entered value: " + inputName.getAttribute("value")); // 🤠
+
 		} finally {
 			if (driver != null) {
 				driver.quit();
