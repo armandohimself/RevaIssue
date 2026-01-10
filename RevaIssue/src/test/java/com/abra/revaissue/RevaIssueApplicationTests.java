028e9d98bd685c3ab76e1eba907fae5ab3194016
armandohimself <hey@armandohimself.com>
2026-01-09 22:23:41 -0600

    Selenium is working

--- RevaIssue/src/test/java/com/abra/revaissue/RevaIssueApplicationTests.java
+++ RevaIssue/src/test/java/com/abra/revaissue/RevaIssueApplicationTests.java
@@ -1,18 +1,29 @@
 package com.abra.revaissue;
 
 import org.junit.jupiter.api.Test;
-// import org.springframework.boot.test.context.SpringBootTest;
-// * Right now @SpringBootTest spins up the entire context (triggering JPA) causing an error.
-
-// * Adding an Assertions for a smoke test, this can be deleted later once we implement actual tests.
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
-// @SpringBootTest
+/**
+ * This is your base test file.
+ */
+@SpringBootTest
 class RevaIssueApplicationTests {
 
 	@Test
 	void contextLoads() {
 		assertThat(2 + 2).isEqualTo(4);
+	}
+
+	public static void main (String[] args) {
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://practicetestautomation.com/practice-test-login/");
+
+		System.out.println(driver.getTitle());
+
 	}
 
 }
