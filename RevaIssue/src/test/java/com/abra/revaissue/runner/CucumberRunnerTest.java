470723b24adb73d32fe3b08abff045be2d354a35
armandohimself <hey@armandohimself.com>
2026-01-22 04:17:26 -0500

    All tests from andrew and roman's branch work

--- RevaIssue/src/test/java/com/abra/revaissue/runner/CucumberRunnerTest.java
+++ RevaIssue/src/test/java/com/abra/revaissue/runner/CucumberRunnerTest.java
@@ -1,10 +1,6 @@
 package com.abra.revaissue.runner;
 
 import org.junit.platform.suite.api.Suite;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import io.cucumber.spring.CucumberContextConfiguration;
-
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.ConfigurationParameter;
@@ -19,6 +15,4 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.abra.revaissue.E2E")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:build/report/cucumber/cucumber.html, json:build/report/cucumber/cucumber.json")
 @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "not @wip")
-@CucumberContextConfiguration // if you don't include CucumberContextConfiguration the SpringBootTest annotation will have no effect
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class CucumberRunnerTest {}
