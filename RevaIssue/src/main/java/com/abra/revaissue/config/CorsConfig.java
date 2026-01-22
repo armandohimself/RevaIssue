e4757e47add9b824a2d68e8c5a12902c2544662f
KdRome <r.petliak12@gmail.com>
2026-01-22 15:18:44 -0500

    fixed cors and .env issues

--- RevaIssue/src/main/java/com/abra/revaissue/config/CorsConfig.java
+++ RevaIssue/src/main/java/com/abra/revaissue/config/CorsConfig.java
@@ -10,7 +10,13 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+            .allowedOrigins(
+                "http://localhost:4200",
+                "http://localhost",
+                "http://localhost:80",
+                "http://127.0.0.1",
+                "http://127.0.0.1:80"
+            )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
